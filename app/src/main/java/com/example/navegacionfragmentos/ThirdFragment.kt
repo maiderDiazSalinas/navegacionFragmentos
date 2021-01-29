3c36dedc16c849f8442957485c33b79c5184ff22
maiderDiazSalinas <maiderdiazsalinas@gmail.com>
2021-01-29 17:41:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/navegacionfragmentos/ThirdFragment.kt
@@ -0,0 +1,33 @@
+package com.example.navegacionfragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class ThirdFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_fragment3_pre).setOnClickListener {
+            findNavController().navigate(R.id.action_thirdFragment_to_SecondFragment)
+        }
+        view.findViewById<Button>(R.id.button_fragment3_next).setOnClickListener {
+            findNavController().navigate(R.id.action_thirdFragment_to_FirstFragment)
+        }
+    }
+
+}
